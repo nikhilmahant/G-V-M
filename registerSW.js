4622ca3c5988bdfdd9f2950ea7417abdb31fbbbc
nikhilmahant <nikhilmahant@users.noreply.github.com>
2026-07-09 14:40:04 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/G-V-M/sw.js', { scope: '/G-V-M/' })})}
